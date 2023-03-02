a4b3c38edbc0f8f69995585f9280e0301d6b0ec3
sathishvj <sathishvj@yahoo.com>
2023-03-02 13:09:15 +0530

    moved the looping over lines into main, and do all checks for a line one after another

--- main.py
+++ main.py
@@ -33,8 +33,7 @@ def to_titlecase(s):
             final.append(word.capitalize())
     return " ".join(final)
 
-def check_words(lines):
-    n=0
+def check_words(line, n):
 # check misspelt/misused words
     do_not_use_words=[
             "gcp" # use Google Cloud instead
@@ -43,71 +42,63 @@ def check_words(lines):
             "BigQuery",
             "Bigtable"
             ]
-    for line in lines:
-        n=n+1
-        line = line.strip()
-        if len(line)==0:
-            continue
-        words=line.split()
-        for word in words:
-            if word.lower() in do_not_use_words:
-                print("Error! Line", n, ": Do not use word: ", word)
-            if word.lower() in (mw.lower() for mw in misspelt_words):
-                if not word in misspelt_words:
-                    print("Error! Line", n, ": Word not correctly written: ", word)
+    line = line.strip()
+    if len(line)==0:
+        return
+    words=line.split()
+    for word in words:
+        if word.lower() in do_not_use_words:
+            print("Error! Line", n, ": Do not use word: ", word)
+        if word.lower() in (mw.lower() for mw in misspelt_words):
+            if not word in misspelt_words:
+                print("Error! Line", n, ": Word not correctly written: ", word)
 
 
 # check format of each line
-def check_line_format(lines):
-    n=0
-    for line in lines:
-        n=n+1
-        line = line.strip()
-        if len(line)==0:
-            continue
-        elif line.startswith("![[/fragments"):
-            # check if fragment exists
-            fragment=line[14:-2]
-            fragments_path=gcp_enablement_content_folder+path.sep+"fragments"+path.sep+fragment
-            if not os.path.isdir(fragments_path):
-                print("Error! Line", n, ": No fragment: ", line, fragments_path)
-        #elif line.startswith("# "):
-            # h1
-        elif line.startswith("## "):
-            # h2
-            if line.startswith("## Task"):
-                #print("# task title")
-                # todo: check if using dot after number.
-                # todo: check if number is proper.
-                # check if title case. Todo: don't hardcode 10.
-                t=line[10:].strip()
-                if not t == to_titlecase(t):
-                    print("Error! Line", n, ": Not title case: ", line, " | Should be: ", to_titlecase(t))
+def check_line_format(line, n):
+    line = line.strip()
+    if len(line)==0:
+        return
+    elif line.startswith("![[/fragments"):
+        # check if fragment exists
+        fragment=line[14:-2]
+        fragments_path=gcp_enablement_content_folder+path.sep+"fragments"+path.sep+fragment
+        if not os.path.isdir(fragments_path):
+            print("Error! Line", n, ": No fragment: ", line, fragments_path)
+    #elif line.startswith("# "):
+        # h1
+    elif line.startswith("## "):
+        # h2
+        if line.startswith("## Task"):
+            #print("# task title")
+            # todo: check if using dot after number.
+            # todo: check if number is proper.
+            # check if title case. Todo: don't hardcode 10.
+            t=line[10:].strip()
+            if not t == to_titlecase(t):
+                print("Error! Line", n, ": Not title case: ", line, " | Should be: ", to_titlecase(t))
 
 
-def check_imgs(lines):
-    n=0
-    for line in lines:
-        n=n+1
-        line = line.strip()
-        if len(line)==0:
-            continue
-        words=line.split()
-        for word in words:
-            if word.startswith('src="img/') or word.startswith("src='img/"):
-                img_name=word[9:-1]
-                img_path=folder+path.sep+"img"+path.sep+img_name
-                if not os.path.isfile(img_path):
-                    print("Error! Line", n, ": Img does not exist: img/"+img_name)
-    #print(line)
+def check_imgs(line, n):
+    line = line.strip()
+    if len(line)==0:
+        return
+    words=line.split()
+    for word in words:
+        if word.startswith('src="img/') or word.startswith("src='img/"):
+            img_name=word[9:-1]
+            img_path=folder+path.sep+"img"+path.sep+img_name
+            if not os.path.isfile(img_path):
+                print("Error! Line", n, ": Img does not exist: img/"+img_name)
 
 # read all the lines from en.md
 file=open(enmd_path)
 lines=file.readlines()
 
-check_words(lines)
-
-check_line_format(lines)
-
-check_imgs(lines)
+n=0
+for line in lines:
+    n=n+1
+    check_words(line, n)
+    check_line_format(line, n)
+    check_imgs(line, n)
 
